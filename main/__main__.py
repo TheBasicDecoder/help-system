a176aa9f1e30f4562f71aa1ef9516e8af477d2b8
Fernando Lopes <118869201+plasmus777@users.noreply.github.com>
2023-07-31 16:02:50 -0300

    Começo da criação da tela de login

--- main/__main__.py
+++ main/__main__.py
@@ -1,15 +1,18 @@
 import tkinter as tk
 from interface import HelpSystemApp
+from gui.tela_login import Tela_Login
 
 def main():
     print('Começo do aplicativo aqui')
     
-    root = tk.Tk()
-    app = HelpSystemApp(root)
-    root.mainloop()
+    tl = Tela_Login()
     
-    hsa = HelpSystemApp(root)
-    hsa.inicializar()
+    #root = tk.Tk()
+    #app = HelpSystemApp(root)
+    #root.mainloop()
+    #hsa = HelpSystemApp(root)
+    #hsa.inicializar()
+    
     #sys.exit(main())
     
 if __name__ == "__main__":
---
+++ main/gui/tela_login.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from PIL import Image, ImageDraw
+
+class Tela_Login:
+    def __init__(self):
+        janela = tk.Tk()
+        janela.title('Tela de Login')
+        janela.config(padx=0, pady=0)
+        
+        background = tk.PhotoImage(file='texturas/Tela de Login/Fundo.png')
+        
+        tk.Label(janela, image=background).pack()
+
+        janela.mainloop()
