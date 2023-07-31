5e2aab6998f58949f17d8b1691c311b338015616
Fernando Lopes <118869201+plasmus777@users.noreply.github.com>
2023-07-31 16:05:24 -0300

    Maximizar janela de login

--- main/gui/tela_login.py
+++ main/gui/tela_login.py
@@ -6,6 +6,7 @@ class Tela_Login:
         janela = tk.Tk()
         janela.title('Tela de Login')
         janela.config(padx=0, pady=0)
+        janela.state('zoomed')
         
         background = tk.PhotoImage(file='texturas/Tela de Login/Fundo.png')
         
