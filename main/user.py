6a7345974c4298ec0def5f723e4c83baf7a75247
Fernando Lopes <118869201+plasmus777@users.noreply.github.com>
2023-08-01 11:18:05 -0300

    Upload de texturas

--- main/user.py
+++ main/user.py
@@ -1,5 +1,13 @@
 class User:
+    email = 'email@usuario.com'
+    nome = 'Usuário'
+    senha = 'usuario123'
+    sobre = 'Um usuário comum'
+    
     def __init__(usuario, email, nome, senha, sobre):
-        print(email)
+        usuario.email = email
+        usuario.nome = nome
+        usuario.senha = senha
+        usuario.sobre = sobre
         
     
